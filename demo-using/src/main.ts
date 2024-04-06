2ffe08aab7690165a170c1fc6f4b4dc4ac66f80e
sniggiw <bewjweng@163.com>
2024-04-06 16:33:08 +0800

    feat:add element-plus and change the style of  demo-using

--- demo-using/src/main.ts
+++ demo-using/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import '@/assets/css/global.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
