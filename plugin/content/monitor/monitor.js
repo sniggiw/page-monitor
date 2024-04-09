5b7c4587573eb439e71622188f07ed43f852751d
sniggiw <bewjweng@163.com>
2024-04-09 22:10:15 +0800

    feat: the style of the select dom and the method of get xpath

--- plugin/content/monitor/monitor.js
+++ plugin/content/monitor/monitor.js
@@ -8,6 +8,8 @@ class Monitor {
     // 修改 this 指向
     this.mouseoverHandler = this.mouseoverHandler.bind(this)
     this.mouseoutHandler = this.mouseoutHandler.bind(this)
+
+    this.xpath = []
   }
 
   // 创建监控面板
@@ -29,6 +31,7 @@ class Monitor {
     cancelBtn.textContent = '取消'
 
     // data- 自定义的属性，在 dom 对象上一律以 dataset 对象方式获取
+    // 后续用于页面自身的点击事件
     confirmBtn.dataset.specialClick = 'true'
     cancelBtn.dataset.specialClick = 'true'
 
@@ -45,8 +48,8 @@ class Monitor {
     })
 
     confirmBtn.addEventListener('click', (event) => {
-      event.stopPropagation()
       if (event.target.classList.contains('monitor_confirm_not')) return
+      event.stopPropagation()
     })
 
     document.body.appendChild(monitor_panel)
@@ -59,6 +62,56 @@ class Monitor {
     const { monitor_type } = params
     this.monitor_panel = this.createMonitorPanel(monitor_type)
     this.addEventListeners()
+
+    // 点击元素时
+    document.addEventListener(
+      'click',
+      (event) => {
+        // 判断是否为元素节点 event.target.nodeType 为 1 时表明是元素节点
+        let element = event.target.nodeType === 1 ? event.target : event.target.parentElement
+        if (!element) return
+
+        // 阻止页面元素本身的点击事件
+        if (!element.dataset.specialClick) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        // 获取元素的 xpath 路径
+        if (!element.dataset.specialClick) {
+          const xpath = `/html/${this.getXPath(event.target)}`
+          const isExistXPath = this.xpath.indexOf(xpath)
+
+          console.log('XPath', this.xpath)
+
+          // toggle active 类
+          if (element.classList.contains('monitor_select_active')) {
+            // 如果存在，则删除该类
+            element.classList.remove('monitor_select_active')
+            this.xpath.splice(isExistXPath, 1)
+          } else {
+            // 因为数值监控时，只能选择一个元素进行监控，因此如果 monitor_type 为数值类型时，需要先将所有的 active 类清除掉再进行添加
+            if (monitor_type == 2) {
+              const allSelectItem = document.querySelectorAll('.monitor_select_active')
+              allSelectItem.forEach((el) => {
+                el.classList.remove('monitor_select_active')
+              })
+            }
+            // 如果不存在，则添加该类
+            element.classList.add('monitor_select_active')
+
+            this.xpath.push(xpath)
+          }
+
+          if (this.xpath.length === 0) {
+            this.confirmBtn.classList.add('monitor_confirm_not')
+          } else {
+            this.confirmBtn.classList.remove('monitor_confirm_not')
+          }
+        }
+      },
+      true // 捕获阶段执行
+    )
   }
 
   // 创建 dom 元素选择框
@@ -89,5 +142,23 @@ class Monitor {
   addEventListeners() {
     document.addEventListener('mouseover', this.mouseoverHandler)
     document.addEventListener('mouseout', this.mouseoutHandler)
+  }
+
+  // 获取 xpath 路径
+  getXPath(element) {
+    if (element === document.body) return element.tagName.toLowerCase()
+
+    // 同类型元素的位置
+    let ix = 0
+
+    const siblings = element.parentNode.childNodes
+
+    for (let i = 0; i < siblings.length; i++) {
+      const sibling = siblings[i]
+
+      if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix++
+
+      if (sibling === element) return this.getXPath(element.parentNode) + '/' + element.tagName.toLowerCase() + '[' + (ix + 1) + ']'
+    }
   }
 }
